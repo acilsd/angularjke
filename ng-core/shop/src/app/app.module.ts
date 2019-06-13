e6e00b7853745561b407840b74c9bb5eae84749f
acilsd <acilsd@derpy.ru>
2019-06-13 21:28:51 +0300

    project structure

--- ng-core/shop/src/app/app.module.ts
+++ ng-core/shop/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { ListComponent } from './shopping-list/list/list.component';
+import { FormComponent } from './shopping-list/form/form.component';
+import { ItemComponent } from './shopping-list/item/item.component';
+import { DetailComponent } from './recipe-list/detail/detail.component';
+import { RecipeComponent } from './recipe-list/recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListComponent,
+    FormComponent,
+    ItemComponent,
+    DetailComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
