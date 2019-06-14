b50b8c4d26323a1262dd1836b47e62a2cfb8996b
acilsd <acilsd@derpy.ru>
2019-06-14 20:44:00 +0300

    add basic routing

--- ng-core/shop/src/app/app-routing.module.ts
+++ ng-core/shop/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RecipeBookComponent },
+  { path: 'shoplist', component: ShoppingListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
