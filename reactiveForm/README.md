5ce7a999691c2d3b5eea5d8c8187ea4026d50904
acilsd <acilsd@derpy.ru>
2019-06-27 22:48:47 +0300

    reactive_forms startup

---
+++ reactiveForm/README.md
@@ -0,0 +1,1 @@
+## ng ReactiveForms trainee code exs
---
+++ reactiveForm/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ArrayFormComponent } from './array-form/array-form.component';
+import { BasicFormComponent } from './basic-form/basic-form.component';
+import { NestedFormComponent } from './nested-form/nested-form.component';
+import { SubmitFormComponent } from './submit-form/submit-form.component';
+import { ValidFormComponent } from './valid-form/valid-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArrayFormComponent,
+    BasicFormComponent,
+    NestedFormComponent,
+    SubmitFormComponent,
+    ValidFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
