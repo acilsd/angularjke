4d2dab1156472085f93bd8014a9c8510c2a51100
acilsd <acilsd@derpy.ru>
2019-07-10 17:29:32 +0300

    startup

--- pipesEx/src/app/app.component.ts
+++ pipesEx/src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipesEx';
+  public hardcodedData = [
+    {
+      type: 'medium',
+      name: 'Prod',
+      status: 'stable',
+      started: new Date(15, 6, 2019)
+    },
+    {
+      type: 'small',
+      name: 'User',
+      status: 'offline',
+      started: new Date(14, 6, 2019)
+    },
+    {
+      type: 'large',
+      name: 'Test',
+      status: 'stable',
+      started: new Date(13, 6, 2019)
+    },
+    {
+      type: 'medium',
+      name: 'CI',
+      status: 'critical',
+      started: new Date(12, 6, 2019)
+    },
+    {
+      type: 'small',
+      name: 'Dev',
+      status: 'offline',
+      started: new Date(11, 6, 2019)
+    }
+  ];
+
+  public getStatusClasses(server: any) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
+  }
 }
