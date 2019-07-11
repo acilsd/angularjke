1a8cac1b3014c1c3a059101f9820d9c44f850ae2
acilsd <acilsd@derpy.ru>
2019-07-11 21:12:11 +0300

    adjust imports

--- udemy/src/app/app.module.ts
+++ udemy/src/app/app.module.ts
@@ -6,10 +6,6 @@ import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './layout/header/header.component';
 
-import { RecipeBookModule } from './recipe-book/recipe-book.module';
-
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
-
 import { DirectivesModule } from './directives/directives.module';
 
 import { ShoppingListService } from './shopping-list/services/shopping-list/shopping-list.service';
@@ -17,13 +13,7 @@ import { RecipeService } from './recipe-book/services/recipes/recipe.service';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RecipeBookModule,
-    ShoppingListModule,
-    DirectivesModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, DirectivesModule],
   bootstrap: [AppComponent],
   providers: [ShoppingListService, RecipeService]
 })
